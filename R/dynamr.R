f1ae648466b8a147d807f2564caa7fcab7079d12
dnkent <kent.249@osu.edu>
2020-07-09 15:39:57 -0400

    PA conditional accept new code

--- R/dynamr.R
+++ R/dynamr.R
@@ -1,4 +1,4 @@
-#' A Randomization Approach to Dynamic Analysis of Panel Data
+#' A Permutation-Based Changepoint Technique for Monitoring Effect Sizes
 #'
 #' \code{dynamr} is used to detect where effect values change temporally when
 #' estimating generalized linear models with panel data.
@@ -7,12 +7,9 @@
 #' @param time_var Name of variable denoting time period for any observation,
 #' must be a character
 #' @param formula Formula for glm object
-#' @param num_models Number of time-specific models to be ran
 #' @param N Number of coefficient samples extracted for calculating stable
 #' statistical behavior, defaults to 2000
 #' @param window_size Number of time periods per model
-#' @param start_time First time period value
-#' @param end_time Last time period value
 #' @param change_var Variable being checked for time-varying behavior,
 #' must be a character
 #' @param family A description of the error distribution to be used for the
@@ -25,7 +22,7 @@
 #'
 #' @examples
 #' library(ISLR)
-#' library(dplyr)
+#' library(dynamr)
 #'
 #' data("Weekly")
 #'
@@ -33,10 +30,7 @@
 #'  dat = Weekly,
 #'  time_var = "Year",
 #'  formula = Today ~ Lag1 + Lag2,
-#'  num_models = 21,
 #'  window_size = 1,
-#'  start_time = 1990,
-#'  end_time = 2010,
 #'  change_var = "Lag2",
 #'  family = "gaussian",
 #'  N = 5000
@@ -44,29 +38,37 @@
 #'
 #' stock_dynam
 #'
-#' stock_dynam %>% filter(change == 1)
-
 dynamr <- function(
   dat,
   time_var,
   formula,
-  num_models,
   N = 2000,
   window_size,
-  start_time,
-  end_time,
   change_var,
   family = c(
     "gaussian",
     "binomial",
     "poisson"
   )
-){
-  # Coefficient vector
-  coefs <- rep(NA, N)
-
+) {
+  # Denote start and end times
+  start_time <- min(dat[[time_var]], na.rm = TRUE)
+  end_time <- max(dat[[time_var]], na.rm = TRUE)
+  
+  # Number of models to fit over time
+  # + 2 works out to include all time periods
+  num_models <- end_time - (start_time + window_size) + 2
+  
+  # Storage for coefficient estimates
+  coefs <- matrix(
+    NA,
+    nrow = N,
+    ncol = length(all.vars(formula[-2])) + 1 # -2 removes DV, +1 intercept
+  )
+  
   # Create dist of time-invariant coefficients
-  for(i in 1:N){
+  # For each variable
+  for (i in 1:N) {
     t <- sample(c(start_time:end_time), window_size)
     keep_obs <- which(dat[[time_var]] %in% t)
     mod <- glm( # model with randomly selected time periods
@@ -75,21 +77,35 @@ dynamr <- function(
       family = family
     )
     ## Which to extract?
-    coef_num <- which(names(coef(mod)) == change_var)
-    coefs[i] <- coef(mod)[coef_num]
+    coefs[i, ] <- coef(mod)
   }
-
-  # Tolerance region
-  region <- 3 * sd(coefs)
-
-  # Change locations
+  
+  # Create ncol tolerance regions (shewhart)
+  region <- 3 * apply(coefs, MARGIN = 2, sd)
+  
+  # 3 columns per var
+  # extract variable name for column names
   changes <- tibble(
-    "Time" = as.integer(),
-    "Estimate" = as.numeric(),
-    "std.err" = as.numeric(),
-    "change" = as.integer()
+    "time" = as.integer(),
+    "int_est" = as.numeric(),
+    "int_sd" = as.numeric(),
+    "pr_int_change" = as.integer()
   )
-
+  
+  # Automate dynamic dynamic by formula terms
+  for (i in 1:length(all.vars(formula[-2]))) {
+    changes <- changes %>%
+      add_column(!!
+                   paste0(all.vars(formula[-2])[i], "_est", sep = ""),
+      ) %>%
+      add_column(!!
+                   paste0(all.vars(formula[-2])[i], "_sd", sep = ""),
+      ) %>%
+      add_column(!!
+                   paste0("pr_", all.vars(formula[-2])[i], "_change", sep = ""),
+      )
+  }
+  
   # Extract coefficients
   for(i in 1:num_models){
     time_min <- (start_time + i) - 1
@@ -104,34 +120,54 @@ dynamr <- function(
       data = temp,
       family = family
     )
-    ## Which to extract?
-    coef_num <- which(names(coef(mod)) == change_var)
-    est <- summary(mod)$coefficients[coef_num, 1:2]
-
-    if(i == 1){ ## Establish starting region
-      mu <- est[1]
-      mu_upper <- mu + region
-      mu_lower <- mu - region
-      # append time -- start  the window
-      changes[i, 1] <- median(seq(time_min, time_max))
-      ## because using a window, use middle time
-      changes[i, 2] <- mu # append estimate
-      changes[i, 3] <- est[2] # append confint
-      changes[i, 4] <- 0 # append changes
+    est <- summary(mod)$coefficients[,1:2]
+    
+    if (i == 1) {
+      mu <- est[, 1] # coefficients
+      mu_upper <- mu + region # upper bounds
+      mu_lower <- mu - region # lower bounds
+      
+      # Append time -- start the window with middle time
+      changes[i, 1] <- floor(median(seq(time_min, time_max)))
+      
+      # Fill in estimates df
+      # Will work for any number of covariates
+      for (j in 1:nrow(est)) {
+        changes[i, 3 * j - 1] <- est[j, 1] # coefficient
+        changes[i, 3 * j] <- est[j, 2] # sd
+        changes[i, 3 * j + 1] <- 0 # first point can"t be change
+      }
     }else{
-      coef_mw <- est[1] ## moving window coefficient
       # append time
-      changes[i, 1] <- median(seq(time_min, time_max))
-      ## because using a window, use middle time
-      changes[i, 2] <- coef_mw # append coefficient
-      changes[i, 3] <- est[2] # append confint
-      if(coef_mw > mu_upper | coef_mw < mu_lower){
-        changes[i, 4] <- 1 # append change
-        mu <- coef_mw
-        mu_upper <- mu + region # set new region
-        mu_lower <- mu - region # set new region
-      }else{
-        changes[i, 4] <- 0 # append change
+      changes[i, 1] <- floor(median(seq(time_min, time_max)))
+      
+      # estimates
+      for (j in 1:nrow(est)) {
+        changes[i, 3 * j - 1] <- est[j, 1] # coefficient
+        changes[i, 3 * j] <- est[j, 2] # sd
+        # pr(observed coef is larger than window"s upper bound)
+        pr_above_upper <- 1 - pnorm(
+          as.numeric(mu_upper[j]),
+          mean = as.numeric(changes[i, 3 * j - 1]),
+          sd = as.numeric(changes[i, 3 * j])
+        )
+        pr_below_lower <- pnorm(
+          as.numeric(mu_lower[j]),
+          mean = as.numeric(changes[i, 3 * j - 1]),
+          sd = as.numeric(changes[i, 3 * j])
+        )
+        # Record prob of change
+        ifelse(
+          pr_above_upper > pr_below_lower,
+          changes[i, 3 * j + 1] <- pr_above_upper,
+          changes[i, 3 * j + 1] <- pr_below_lower
+        )
+        # update bounds if so
+        if (pr_above_upper >= 0.5 | pr_below_lower >= 0.5) {
+          mu[j] <- as.numeric(changes[i, 3 * j - 1]) # is a change
+          mu_upper[j] <- mu[j] + region[j]
+          mu_lower[j] <- mu[j] - region[j]
+        }
       }
     }
   }
